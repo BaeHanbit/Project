01afff1bbe7ff7d8fea9ef3f572460ffd3a7c882
BaeHanbit <haun24ne@gmail.com>
2018-08-26 19:54:20 +0900

    Show me the menu V0.01

--- 쇼미더메뉴_프로젝트/show_me_the_menu.py
+++ 쇼미더메뉴_프로젝트/show_me_the_menu.py
@@ -1,14 +1,18 @@
-import requests
+from selenium import webdriver
 from bs4 import BeautifulSoup
 
-URL = "http://dsm2015.cafe24.com/#/"
-response = requests.get(URL)
+# 현재 크롬 위치를 지정 후, 새 크롬창을 띄워준다.
+driver = webdriver.Chrome('C:\chromedriver_win32\chromedriver.exe')
+# 해당 URL로 이동한다.
+driver.get('http://dsm2015.cafe24.com/#/')
 
-soup = BeautifulSoup(response.text,'lxml')
 
-test=list()
-test02=list()
+html = driver.page_source
+soup = BeautifulSoup(html,'lxml')
 
-test=soup.find_all('div',{"id":"app"})
-for i in test:
-    print(i)
+meal_list = soup.find_all('div',{"class":"meal"})
+
+for i in meal_list:
+    print(i.get_text())
+    
+driver.quit()
