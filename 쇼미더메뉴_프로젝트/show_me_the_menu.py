b2a712979926c488ef8633d60c55e29df7f2c468
BaeHanbit <haun24ne@gmail.com>
2018-08-27 13:29:55 +0900

    Add mongoDB

--- 쇼미더메뉴_프로젝트/show_me_the_menu.py
+++ 쇼미더메뉴_프로젝트/show_me_the_menu.py
@@ -1,18 +1,26 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
+import pymongo
 
-# 현재 크롬 위치를 지정 후, 새 크롬창을 띄워준다.
+#############################웹 페이지에서 정보 받아오기############################
 driver = webdriver.Chrome('C:\chromedriver_win32\chromedriver.exe')
-# 해당 URL로 이동한다.
 driver.get('http://dsm2015.cafe24.com/#/')
-
 
 html = driver.page_source
 soup = BeautifulSoup(html,'lxml')
 
 meal_list = soup.find_all('div',{"class":"meal"})
-
-for i in meal_list:
-    print(i.get_text())
-    
 driver.quit()
+#############################웹 페이지에서 정보 받아오기############################
+#############################몽고 디비 연계하여 데이터 입력하기#####################\
+key=1
+
+connection = pymongo.MongoClient("localhost", 27017)
+db = connection.Menu
+collection=db.Breakfast
+
+for i in meal_list[0]:
+    collection.insert_one({str(key):str(i.get_text())})
+    
+
+#############################몽고 디비 연계하여 데이터 입력하기#####################
