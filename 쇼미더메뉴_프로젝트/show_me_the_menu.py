b4d49c4c50b30283f5e0c1b6ebf211459ad77264
BaeHanbit <haun24ne@gmail.com>
2018-08-28 16:54:02 +0900

    2018-08-28

--- 쇼미더메뉴_프로젝트/show_me_the_menu.py
+++ 쇼미더메뉴_프로젝트/show_me_the_menu.py
@@ -1,26 +1,33 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
+import json
+from datetime import datetime
+import requests
 import pymongo
 
 #############################웹 페이지에서 정보 받아오기############################
-driver = webdriver.Chrome('C:\chromedriver_win32\chromedriver.exe')
-driver.get('http://dsm2015.cafe24.com/#/')
-
-html = driver.page_source
-soup = BeautifulSoup(html,'lxml')
-
-meal_list = soup.find_all('div',{"class":"meal"})
-driver.quit()
+if(datetime.today().month<10):
+    URL = "http://dsm2015.cafe24.com/v2/meal/" + str(datetime.today().year) + "-0" + str(datetime.today().month) + '-' + str(datetime.today().day)
+else:
+    URL = "http://dsm2015.cafe24.com/v2/meal/" + str(datetime.today().year) + '-' + str(datetime.today().month) + '-' + str(datetime.today().day)
+res=requests.get(URL)
+meals=json.loads(res.text)
 #############################웹 페이지에서 정보 받아오기############################
-#############################몽고 디비 연계하여 데이터 입력하기#####################\
-key=1
-
+#############################몽고 디비 연계하여 데이터 입력하기#####################
 connection = pymongo.MongoClient("localhost", 27017)
 db = connection.Menu
-collection=db.Breakfast
+collection = db.meal
 
-for i in meal_list[0]:
-    collection.insert_one({str(key):str(i.get_text())})
-    
-
+num=1
+for i in meals:
+    collection.insert_one({str(i):meals[i]})
+    num+=1
 #############################몽고 디비 연계하여 데이터 입력하기#####################
+#############################몽고 디비 연계하여 데이터 받아오기#####################
+collection = db.emp
+docs = collection.find()
+
+for i in docs:
+    food=docs[i]
+
+print(food)
